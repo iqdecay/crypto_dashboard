52eecb724733765399ce4e3dcace6119b7638c2c
vnepveu <victor.nepveu@imt-atlantique.net>
2020-03-24 16:01:31 -0400

    Change url handling

--- query_prices.py
+++ query_prices.py
@@ -3,12 +3,21 @@ import yaml
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 API_KEY_FILE = "auth.yaml"
-cmc_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-cmc_url_fiat = "https://pro-api.coinmarketcap.com/v1/tools/price-conversion"
+cmc_url = "https://pro-api.coinmarketcap.com/v1/"
+endpoint_crypto = "cryptocurrency/quotes/latest"
+endpoint_fiat = "tools/price-conversion"
+endpoint_symbols = "cryptocurrency/map"
 
 with open(API_KEY_FILE, 'r') as api_key_file:
     api_key = yaml.safe_load(api_key_file)
 cmc_key = api_key[0]["coinmarketcap"]
+
+
+def get_available_symbols():
+    params = {
+        "CMC_PRO_API_KEY": cmc_key,
+    }
+    r = requests.get(cmc_url + endpoint_symbols, params)
 
 
 def get_quotes(symbol):
@@ -17,7 +26,7 @@ def get_quotes(symbol):
         "symbol": symbol
     }
     try:
-        r = requests.get(cmc_url, params)
+        r = requests.get(cmc_url + endpoint_crypto, params)
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         raise e
     if r.status_code == 200:
@@ -37,6 +46,6 @@ def get_fiat_conversion(source_symbol, target_symbol):
               "symbol": source_symbol,
               "convert": [target_symbol]}
 
-    r = requests.get(cmc_url_fiat, params)
+    r = requests.get(cmc_url + endpoint_fiat, params)
     rate = r.json()["data"]["quote"][target_symbol]["price"]
     return rate
